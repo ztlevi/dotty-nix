379cb015c7a167295073b2104f2b34fee8e76f11
Ting Zhou <ztlevi.work@gmail.com>
2020-05-30 11:39:53 -0700

    fzf: bind key

--- config/fzf/aliases.zsh
+++ config/fzf/aliases.zsh
@@ -1,10 +1,7 @@
-bindkey '^[x' fzf-history-widget
-bindkey '^[p' fzf-file-widget
-
-if _is_callable fzf-share; then
-  source "$(fzf-share)/key-bindings.zsh"
-  source "$(fzf-share)/completion.zsh"
-fi
+bindkey -M viins '^[x' fzf-history-widget
+bindkey -M viins '^[p' fzf-file-widget
+bindkey -M viins "^r" fzf-history-widget
+bindkey "^I" fzf-completion
 
 for file in ${XDG_CONFIG_HOME}/fzf/addons/*.zsh; do
   source ${file}
