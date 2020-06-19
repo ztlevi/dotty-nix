5f0e85771a160bb80f981523c6a5bcec33f3c676
Ting Zhou <ztlevi.work@gmail.com>
2020-06-19 01:30:57 -0700

    fcitx: add zhwiki dict

--- modules/services/fcitx.nix
+++ modules/services/fcitx.nix
@@ -11,12 +11,18 @@ with lib; {
     i18n.inputMethod.enabled = "fcitx";
     i18n.inputMethod.fcitx.engines = with pkgs.fcitx-engines; [ rime ];
     # Fcitx configs need to be writable
-    my.home.home.activation.downloadSougouDict = ''
+    my.home.home.activation.setupRimeConfig = ''
       mkdir -p $XDG_CONFIG_HOME/fcitx/rime
       ln -s -f $DOTFILES/config/fcitx/config $XDG_CONFIG_HOME/fcitx/
       ln -s -f $DOTFILES/config/fcitx/profile $XDG_CONFIG_HOME/fcitx/
-      ln -s -f $DOTFILES/config/fcitx/rime/*.custom.yaml $XDG_CONFIG_HOME/fcitx/rime/
+      ln -s -f $DOTFILES/config/fcitx/rime/*.yaml $XDG_CONFIG_HOME/fcitx/rime/
       ln -s -f $DOTFILES/assets/rime-dictionaries/*.dict.yaml $XDG_CONFIG_HOME/fcitx/rime/
+
+      # Download zhwiki pinyin dictionary
+      if [[ ! -f $XDG_CONFIG_HOME/fcitx/rime/zhwiki.dict.yaml ]]; then
+        ${pkgs.wget}/bin/wget --no-check-certificate https://github.com/felixonmars/fcitx5-pinyin-zhwiki/releases/download/0.2.1/zhwiki-20200601.dict.yaml \
+          -O $XDG_CONFIG_HOME/fcitx/rime/zhwiki.dict.yaml
+      fi
     '';
 
   };
