3389572aa3b854520c67805eb4aa10961509e35e
Ting Zhou <ztlevi.work@gmail.com>
2020-12-22 00:06:45 -0800

    services: update ssh keys

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -16,7 +16,7 @@
       };
 
       user.openssh.authorizedKeys.keys = [
-        # "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8Tl2pj6CPBV3V72cohPhn+k0a/Na3cOrbmUtFC7rR7icWocSs1S1cSrP3sPLwUqRh3+zfIuLXTrTv3gj8Xvg8WELOspzsiYeAtbcHrnWnx/a7xzYvFyT9/8hkiGM/F6w7IuKEk+AZW34vARSgRPJ1FdH8NbPKJ8ay9zW9XB9YJGnbzIRmsVVpQ8l6Fh8ZqRjZfC1ea7hns8+HgjPrIHFb+S3qZZiwU4Gc8aWJy9ziwwkllEsSchv3aigYA3eOeW0FUQFiKsLGxbX2b2b3d6jFO4Pu+dMSen0h5IzBo0nh7UADSfJPdwbZaMuJzviKe2y6zg6jaM9XRIhLBT6bftDr henrik@lissner.net"
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCcK44geKeDenAUiie45J7SEYtTWZ7dj/RseitSz/uLiGjpFnrwc6fYcta9iya2SfNDLOfMx9gaT4pBFhytMuaWhxPPAmvrm6Q0OzhvheZCFj2vOSmbV+T0lt4Yama+N9Td9/u/n6mzeXhTZvg+GOOwghDv+ItgP7E/NXDEbJu+H0kS52T0GD3Do/j27qN7z6YxIZCNEF4cMvwMGuWKfDC4FoyCSndQoH+emU09YkRmEnPIWe9H1hG14OHUvpvHisdSk66O+l0w1OXCMQaz63mC/THUVgbgiji3IPqij5YArjZGOpcpEvZi5FgDPGy/u3MM5gv1xHai+HSXah+luf7qyvWg7uI9w3pka7SVpxTF6kGZTFNXcFYhsA1EsWJDS66LCbnx/bDUVQm6o1K0/lUpnSSbC+scRzqI5854z1fS46YW/0X3xnB/GoC8hN/hfm0AaQ46/cQ8N+nBnTBVotejGXJRI+/74q130GeQFjoP1VNfIGpOR9JMxw8U3e12flU= ztlevi@Tings-macbook.local"
       ];
     };
   }
