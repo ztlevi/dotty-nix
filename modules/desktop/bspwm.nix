b80328a233e00c2a31824bdae055c15da9bce870
Ting Zhou <ztlevi.work@gmail.com>
2021-01-15 00:40:44 -0800

    bspwm: update session profile

--- modules/desktop/bspwm.nix
+++ modules/desktop/bspwm.nix
@@ -101,25 +101,27 @@ in {
         displayManager.defaultSession = "none+bspwm";
 
         displayManager.sessionCommands = ''
-          # Trigger autorandr manually because the service does not work
-          ${pkgs.autorandr}/bin/autorandr --change || echo "No autorandr profile found!"
+          if echo ''${XDG_SESSION_DESKTOP} | grep -qi "bspwm"; then
+            # Trigger autorandr manually because the service does not work
+            ${pkgs.autorandr}/bin/autorandr --change || echo "No autorandr profile found!"
 
-          # xrandr --output DisplayPort-0 --primary --auto  --output DisplayPort-1 --auto --right-of DisplayPort-0 --dpi 144
-          RESOLUTION=$(xrandr -q | grep primary | grep ' connected' | cut -d' ' -f4 | cut -d 'x' -f1)
-          if [ -z $RESOLUTION ]; then RESOLUTION=1920; fi
-          export GDK_SCALE=$(($RESOLUTION / 1920))
-          export QT_AUTO_SCREEN_SCALE_FACTOR=0
-          export QT_SCALE_FACTOR=$GDK_SCALE
+            # xrandr --output DisplayPort-0 --primary --auto  --output DisplayPort-1 --auto --right-of DisplayPort-0 --dpi 144
+            RESOLUTION=$(xrandr -q | grep primary | grep ' connected' | cut -d' ' -f4 | cut -d 'x' -f1)
+            if [ -z $RESOLUTION ]; then RESOLUTION=1920; fi
+            export GDK_SCALE=$(($RESOLUTION / 1920))
+            export QT_AUTO_SCREEN_SCALE_FACTOR=0
+            export QT_SCALE_FACTOR=$GDK_SCALE
 
-          # revert gnome screen scale settings
-          command -v gsettings >/dev/null && gsettings set org.gnome.desktop.interface scaling-factor 1
+            # revert gnome screen scale settings
+            command -v gsettings >/dev/null && gsettings set org.gnome.desktop.interface scaling-factor 1
 
-          if command -v gnome-keyring-daemon >/dev/null; then
-            eval $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg)
-            export SSH_AUTH_SOCK
+            if command -v gnome-keyring-daemon >/dev/null; then
+              eval $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg)
+              export SSH_AUTH_SOCK
+            fi
+
+            ${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1 &
           fi
-
-          ${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1 &
         '';
       };
     };
