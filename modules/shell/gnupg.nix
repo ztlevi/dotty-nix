a32027fc1d29c465c08bca7e4d18ac575d73b6f3
Ting Zhou <ztlevi.work@gmail.com>
2021-09-06 19:30:59 -0700

    BUMP config

--- modules/shell/gnupg.nix
+++ modules/shell/gnupg.nix
@@ -20,7 +20,7 @@ in {
       [ "${config.dotfiles.configDirBackupDir}/gpg/aliases.zsh" ];
 
     system.userActivationScripts.gnupgInit = ''
-      [[ ! -f $HOME/.gnupg/pubring.kbx]] && ''${DOTTY_CONFIG_HOME}/misc/gpg/gpg_import.sh
+      [[ ! -f $HOME/.gnupg/pubring.kbx ]] && $DOTTY_CONFIG_HOME/misc/gpg/gpg_import.sh
 
       # https://github.com/microsoft/Git-Credential-Manager-Core/blob/main/docs/linuxcredstores.md
       if [[ ! -f $HOME/.local/share/password-store/.gpg-id ]]; then
