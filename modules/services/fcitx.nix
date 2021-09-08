5601b84960d30fb87c3facd7cb5551d051033b76
Ting Zhou <ztlevi.work@gmail.com>
2021-09-08 08:39:46 -0700

    services/fcitx: remove redundant add-on

--- modules/services/fcitx.nix
+++ modules/services/fcitx.nix
@@ -8,12 +8,7 @@ in {
   config = mkIf cfg.enable {
     i18n.inputMethod = {
       enabled = "fcitx5";
-      fcitx5.addons = with pkgs; [
-        fcitx5-rime
-        fcitx5-chinese-addons
-        fcitx5-gtk
-        libsForQt5.fcitx5-qt
-      ];
+      fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-chinese-addons ];
     };
   };
 }
