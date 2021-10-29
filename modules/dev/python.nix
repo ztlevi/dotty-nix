cb8d5bc10083f4ac60850ecb3b1e8abc2c0b360e
Ting Zhou <ztlevi.work@gmail.com>
2021-10-29 11:19:02 -0700

    dev/python: add python 3.9

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -15,7 +15,7 @@ in {
     user.packages = with pkgs; [
       python38
       # You can install multiple python versions, the first one installed will be mapped as global python3
-      # python39
+      python39
       jetbrains.pycharm-professional
       python38Packages.pip
       python38Packages.grip
