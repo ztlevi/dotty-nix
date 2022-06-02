c91ad7c94628b844595fc03e8ecd37328dc344a9
Ting Zhou <ztlevi.work@gmail.com>
2022-06-01 21:56:36 -0700

    editor: vscode add desktop exec to support wayland

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -5,5 +5,18 @@ let cfg = config.modules.editors.vscode;
 in {
 
   options.modules.editors.vscode = { enable = mkBoolOpt false; };
-  config = mkIf cfg.enable { user.packages = with pkgs; [ vscode ]; };
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      vscode
+      (makeDesktopItem {
+        name = "VSCode";
+        desktopName = "VSCode";
+        genericName = "VSCode";
+        icon = "visual-studio-code";
+        exec =
+          "${vscode}/bin/code --enable-features=UseOzonePlatform --ozone-platform=wayland";
+        categories = [ "Development" ];
+      })
+    ];
+  };
 }
